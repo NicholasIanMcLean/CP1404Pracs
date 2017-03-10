57895540bf024c39014e74516c39113ddef27455
nicholasmclean <nicholas.mclean1@my.jcu.edu.au>
2017-03-10 14:51:46 +1000

    Fixed 3rd question

--- Prac2/asciiTable.py
+++ Prac2/asciiTable.py
@@ -1,21 +1,17 @@
 LOWER = 33
 UPPER = 127
 
-character = input("Enter a character: ")
-convertedCharacter = ord(character)
-print("The ASCII code for", character, "is", convertedCharacter)
+# character = input("Enter a character: ")
+# convertedCharacter = ord(character)
+# print("The ASCII code for", character, "is", convertedCharacter)
+#
+# stringFormatted = "Enter a number between {} to {}: ".format(LOWER, UPPER)
+# number = int(input(stringFormatted))
+# while number < LOWER or number > UPPER:
+#     number = int(input(stringFormatted))
+# convertedNumber = chr(number)
+# print("The ASCII code for", number, "is", convertedNumber)
 
-stringFormatted = "Enter a number between {} to {}".format(LOWER, UPPER)
-number = int(input(stringFormatted))
-while number < LOWER or number > UPPER:
-    number = int(input(stringFormatted))
-convertedNumber = chr(number)
-print("The ASCII code for", number, "is", convertedNumber)
-
-number = LOWER
-startPoint = LOWER
-endPoint = UPPER
-while LOWER <= number <= UPPER:
+for number in range(33, 127, 1):
     character = chr(number)
     print("{:3} : {}".format(number, character))
-    number += 1
