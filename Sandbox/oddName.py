7db9473131c5f308d30bef800db71c8dcdd5c635
nicholasmclean <nicholas.mclean1@my.jcu.edu.au>
2017-03-17 11:38:22 +1000

    adding oddName file

--- Sandbox/oddName.py
+++ Sandbox/oddName.py
@@ -1,3 +1,11 @@
 """
 Nick McLean
 """
+
+name = input("Name? ")
+while name == "":
+    print("cannot be blank")
+    name = input("Name?: ")
+
+for letter in range(1, len(name), 2):
+    print(name[letter])
