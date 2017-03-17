944b548716d97bfdf06bb2c25dbbeddbfcbeb0b9
nicholasmclean <nicholas.mclean1@my.jcu.edu.au>
2017-03-17 11:29:56 +1000

    idk

--- Prac1/temps.py
+++ Prac1/temps.py
@@ -5,16 +5,25 @@ Pseudocode for temperature conversion
 MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celsius_convert():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+def fahrenheit_convert():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = (fahrenheit - 32) * (5 / 9)
+    print("Result: {:.2f} C".format(celsius))
+
+
 while choice != "Q":
     if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
+        celsius_convert()
     elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = (fahrenheit - 32) * (5 / 9)
-        print("Result: {:.2f} C".format(celsius))
-
+        fahrenheit_convert()
     else:
         print("Invalid option")
     print(MENU)
--- Prac2/asciiTable.py
+++ Prac2/asciiTable.py
@@ -1,16 +1,22 @@
 LOWER = 33
 UPPER = 127
 
-# character = input("Enter a character: ")
-# convertedCharacter = ord(character)
-# print("The ASCII code for", character, "is", convertedCharacter)
-#
-# stringFormatted = "Enter a number between {} to {}: ".format(LOWER, UPPER)
-# number = int(input(stringFormatted))
-# while number < LOWER or number > UPPER:
-#     number = int(input(stringFormatted))
-# convertedNumber = chr(number)
-# print("The ASCII code for", number, "is", convertedNumber)
+character = input("Enter a character: ")
+convertedCharacter = ord(character)
+print("The ASCII code for", character, "is", convertedCharacter)
+
+
+def get_number():
+    global number
+    string_formatted = "Enter a number between {} to {}: ".format(LOWER, UPPER)
+    number = int(input(string_formatted))
+    while number < LOWER or number > UPPER:
+        number = int(input(string_formatted))
+
+
+get_number()
+convertedNumber = chr(number)
+print("The ASCII code for", number, "is", convertedNumber)
 
 for number in range(LOWER, UPPER+1):
     character = chr(number)
