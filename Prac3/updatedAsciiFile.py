71b9b2607435efa2f8b80fea6df594664085d0d7
nicholasmclean <nicholas.mclean1@my.jcu.edu.au>
2017-03-17 13:51:04 +1000

    added integar checking onto ascii file

--- Prac3/updatedAsciiFile.py
+++ Prac3/updatedAsciiFile.py
@@ -1,20 +1,27 @@
 LOWER = 33
 UPPER = 127
 
-get_number()
+character = input("Enter a character: ")
+convertedCharacter = ord(character)
+print("The ASCII code for", character, "is", convertedCharacter)
+
+def get_number():
+    valid_input = False
+    while not valid_input:
+        try:
+            string_formatted = "Enter a number between {} to {}: ".format(LOWER, UPPER)
+            number = int(input(string_formatted))
+            while number < LOWER or number > UPPER:
+                number = int(input(string_formatted))
+            valid_input = True
+            return number
+        except ValueError:
+            print("Invalid (not an integer)")
+
+number = get_number()
 convertedNumber = chr(number)
 print("The ASCII code for", number, "is", convertedNumber)
 
-def get_number():
-    global number
-    try:
-        string_formatted = "Enter a number between {} to {}: ".format(LOWER, UPPER)
-        number = int(input(string_formatted))
-        while number < LOWER or number > UPPER:
-            number = int(input(string_formatted))
-        return number
-    except ValueError:
-        print("try again")
-
-
-
+for number in range(LOWER, UPPER+1):
+    character = chr(number)
+    print("{:3} : {}".format(number, character))
